7e3aea188058b3899f28e4d99c497a62e8ef351b
c0deslave49 <cory@dataskeptic.com>
2018-11-08 11:16:47 -0800

    fixed exports

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 var Skeptic = require('./lib');
+var Client = Skeptic.Client;
+var Chat = Skeptic.Chat;
+var Header = Skeptic.Header;
+var Body = Skeptic.Body;
+var Message = Skeptic.Message;
+var Footer = Skeptic.Footer;
 
 module.exports = Skeptic;
+module.exports = Client;
+module.exports = Chat;
+module.exports = Header;
+module.exports = Body;
+module.exports = Message;
+module.exports = Footer;
